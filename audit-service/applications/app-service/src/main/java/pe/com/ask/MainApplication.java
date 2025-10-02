dadad9bcf72f877e81a4ae41b58c37c80e9b15d2
AllanSagastegui <xpheri035@gmail.com>
2025-10-01 21:52:32 -0500

    feat(audit-service): Implementando Eureka

--- audit-service/applications/app-service/src/main/java/pe/com/ask/MainApplication.java
+++ audit-service/applications/app-service/src/main/java/pe/com/ask/MainApplication.java
@@ -3,9 +3,11 @@ package pe.com.ask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
